6a4ee6a773aae312b60a3881fc253a0ae882631c
Tyn Soltys <tyn@tyns-mbp.lan>
2020-08-27 20:44:19 -0400

    fix hover zones & do spacing

--- src/App.js
+++ src/App.js
@@ -29,18 +29,18 @@ function App() {
       }
     }
 
-    const block_a = new Block("a","#33BCAD", "#33BCAD","1");
-    const block_b = new Block("b","#FEFF00", "#FEFF00","2");
-    const block_c = new Block("c","#8781BD", "#8781BD","3");
-    const block_d = new Block("d","#F58221", "#F58221","4");
-    const block_e = new Block("e","#8ED8F7", "#8ED8F7","5");
-    const block_f = new Block("f","#F6ADCD", "#F6ADCD","6");
-    const block_g = new Block("h","#583F99", "#583F99","7");
-    const block_h = new Block("g","#EB008C", "#EB008C","8");
-    const block_i = new Block("i","#03AEEF", "#03AEEF","9");
-    const block_j = new Block("j","#eeeeee", "#33BCAD","0");
+    const block_a = new Block("a","#33BCAD", "#33BCAD","1", "split");
+    const block_b = new Block("b","#FEFF00", "#FEFF00","2", "split dark");
+    const block_c = new Block("c","#8781BD", "#8781BD","3", "split");
+    const block_d = new Block("d","#F58221", "#F58221","4", "split");
+    const block_e = new Block("e","#8ED8F7", "#8ED8F7","5", "split");
+    const block_f = new Block("f","#F6ADCD", "#F6ADCD","6", "split");
+    const block_g = new Block("h","#583F99", "#583F99","7", "split");
+    const block_h = new Block("g","#EB008C", "#EB008C","8", "split");
+    const block_i = new Block("i","#03AEEF", "#03AEEF","9", "split");
+    const block_j = new Block("j","#eeeeee", "#33BCAD","0", "split dark-letter");
     const block_k = new Block("k","#33BCAD", "#33BCAD");
-    const block_l = new Block("l","#FEFF00", "#33BCAD");
+    const block_l = new Block("l","#FEFF00", "#33BCAD","","dark-letter");
     const block_m = new Block("m","#8781BD", "#33BCAD");
     const block_n = new Block("n","#F58221", "#33BCAD");
     const block_o = new Block("o","#8ED8F7", "#33BCAD");
@@ -48,13 +48,13 @@ function App() {
     const block_q = new Block("q","#583F99", "#33BCAD");
     const block_r = new Block("r","#EB008C", "#33BCAD");
     const block_s = new Block("s","#03AEEF", "#33BCAD");
-    const block_t = new Block("t","#eeeeee", "#FEFF00");
-    const block_u = new Block("u","#33BCAD", "#FEFF00");
-    const block_v = new Block("v","#FEFF00", "#FEFF00");
-    const block_w = new Block("w","#8781BD", "#FEFF00");
-    const block_x = new Block("x","#F58221", "#FEFF00");
-    const block_y = new Block("y","#8ED8F7", "#FEFF00");
-    const block_z = new Block("z","#F6ADCD", "#FEFF00");
+    const block_t = new Block("t","#eeeeee", "#FEFF00","","dark-letter");
+    const block_u = new Block("u","#33BCAD", "#FEFF00","","dark-letter");
+    const block_v = new Block("v","#FEFF00", "#FEFF00","","dark-letter");
+    const block_w = new Block("w","#8781BD", "#FEFF00","","dark-letter");
+    const block_x = new Block("x","#F58221", "#FEFF00","","dark-letter");
+    const block_y = new Block("y","#8ED8F7", "#FEFF00","","dark-letter");
+    const block_z = new Block("z","#F6ADCD", "#FEFF00","","dark-letter");
     // All of the below are dupes of A-k
     const block_0 = new Block("0","#eeeeee", "#33BCAD");
     const block_1 = new Block("1","#33BCAD", "#33BCAD");
@@ -66,7 +66,7 @@ function App() {
     const block_7 = new Block("7","#583F99", "#583F99");
     const block_8 = new Block("8","#EB008C", "#EB008C");
     const block_9 = new Block("9","#03AEEF", "#03AEEF");
-    const block_space = new Block("space", "#C4C9CF", "#000000", "", "space");
+    const block_space = new Block("space", "#C4C9CF", "#C4C9CF", "", "space");
     const resultField = document.getElementById("result-field");
 
 
@@ -138,8 +138,10 @@ function App() {
 
 
             <div className="result-zone">
-            
-              <input type="text" id="result-field" className="result-field" value={result} onClick={() => console.log(resultField)}/>
+            <div class='octagon'>
+              <div class='inner'></div>
+            </div>
+              <input type="text" id="result-field" disabled className="result-field" value={result} onClick={() => console.log(resultField)}/>
               <button className="backspace-button" id="backspace-button" onClick={() => backspace()}>backspace</button>
               <button className="clear-button" id="clear-button" onClick={() => setResult("")}>clear</button>
               <button className="copy-button" id="copy-button" onClick={() => setResult("")}>copy</button>
